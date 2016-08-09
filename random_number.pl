223b9aaaf01a971fe612be42255eb03a060b2893
Constantine Diamantis <cdiam@Constantines-MacBook-Pro.local>
2016-08-09 22:58:31 +0300

    commit two lines for print

--- random_number.pl
+++ random_number.pl
@@ -7,3 +7,5 @@ my $rndnum = rand(10);
 print "Decimal part of $rndnum => ". int($rndnum)."\n";
 print "Rounding up to integer of  $rndnum => ". ceil($rndnum)."\n";
 print "Rounding down  to integer of  $rndnum => ". floor($rndnum)."\n";
+print "hello again";
+print "hello there";
