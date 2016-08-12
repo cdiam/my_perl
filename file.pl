6b02e94361545443c0e7adc4e0b38ef2478f3f9d
Constantine <cdiam@windowslive.com>
2016-08-13 00:14:09 +0300

    new line

--- file.pl
+++ file.pl
@@ -10,7 +10,7 @@ print "What is your company?\n\n" ;
 $company = <STDIN> ;
 
 chop ($company) ;
-line 600 "My Line"
+print "line 600 My Line";
 # warn "Waring";
 print "Hello, $name from $company!" ;
 # die "Fatal"
