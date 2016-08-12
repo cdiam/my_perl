adf01fe2193f8487209f1644fc7c3abb252bb19f
Constantine <cdiam@windowslive.com>
2016-08-13 00:06:01 +0300

    comt 2 lines

--- file.pl
+++ file.pl
@@ -10,8 +10,8 @@ print "What is your company?\n\n" ;
 $company = <STDIN> ;
 
 chop ($company) ;
-# line 600 "My Line"
+line 600 "My Line"
 # warn "Waring";
-# print "Hello, $name from $company!" ;
+print "Hello, $name from $company!" ;
 # die "Fatal"
 #
